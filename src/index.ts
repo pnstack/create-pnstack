d1baefe61f5d76b2f7ad73d3c9f1e9dc71f3b196
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-05 16:51:06 +0700

    'up'

---
+++ src/index.ts
@@ -0,0 +1,214 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import spawn from "cross-spawn";
+import minimist from "minimist";
+import prompts from "prompts";
+import {
+  blue,
+  cyan,
+  green,
+  lightGreen,
+  lightRed,
+  magenta,
+  red,
+  reset,
+  yellow,
+} from "kolorist";
+import { TEMPLATES, FRAMEWORKS, Framework, FrameworkType } from "./templates";
+import {
+  copy,
+  emptyDir,
+  formatTargetDir,
+  isEmpty,
+  isValidPackageName,
+  pkgFromUserAgent,
+  setupNodeProject,
+  setupReactSwc,
+  toValidPackageName,
+} from "./utils";
+
+// Avoids autoconversion to number of the project name by defining that the args
+// non associated with an option ( _ ) needs to be parsed as a string. See #4606
+const argv = minimist<{
+  t?: string;
+  template?: string;
+}>(process.argv.slice(2), { string: ["_"] });
+const cwd = process.cwd();
+
+const renameFiles: Record<string, string | undefined> = {
+  _gitignore: ".gitignore",
+};
+
+const defaultTargetDir = "vite-project";
+
+async function init() {
+  const argTargetDir = formatTargetDir(argv._[0]);
+  const argTemplate = argv.template || argv.t;
+
+  let targetDir = argTargetDir || defaultTargetDir;
+  const getProjectName = () =>
+    targetDir === "." ? path.basename(path.resolve()) : targetDir;
+
+  let result: prompts.Answers<
+    "projectName" | "overwrite" | "packageName" | "framework" | "variant"
+  >;
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: argTargetDir ? null : "text",
+          name: "projectName",
+          message: reset("Project name:"),
+          initial: defaultTargetDir,
+          onState: (state) => {
+            targetDir = formatTargetDir(state.value) || defaultTargetDir;
+          },
+        },
+        {
+          type: () =>
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : "confirm",
+          name: "overwrite",
+          message: () =>
+            (targetDir === "."
+              ? "Current directory"
+              : `Target directory "${targetDir}"`) +
+            ` is not empty. Remove existing files and continue?`,
+        },
+        {
+          type: (_, { overwrite }: { overwrite?: boolean }) => {
+            if (overwrite === false) {
+              throw new Error(red("✖") + " Operation cancelled");
+            }
+            return null;
+          },
+          name: "overwriteChecker",
+        },
+        {
+          type: () => (isValidPackageName(getProjectName()) ? null : "text"),
+          name: "packageName",
+          message: reset("Package name:"),
+          initial: () => toValidPackageName(getProjectName()),
+          validate: (dir) =>
+            isValidPackageName(dir) || "Invalid package.json name",
+        },
+        {
+          type:
+            argTemplate && TEMPLATES.includes(argTemplate) ? null : "select",
+          name: "framework",
+          message:
+            typeof argTemplate === "string" && !TEMPLATES.includes(argTemplate)
+              ? reset(
+                  `"${argTemplate}" isn't a valid template. Please choose from below: `
+                )
+              : reset("Select a framework:"),
+          initial: 0,
+          choices: FRAMEWORKS.map((framework) => {
+            const frameworkColor = framework.color;
+            return {
+              title: frameworkColor(framework.display || framework.name),
+              value: framework,
+            };
+          }),
+        },
+        {
+          type: (framework: Framework) =>
+            framework && framework.variants ? "select" : null,
+          name: "variant",
+          message: reset("Select a variant:"),
+          choices: (framework: Framework) =>
+            framework.variants.map((variant) => {
+              const variantColor = variant.color;
+              return {
+                title: variantColor(variant.display || variant.name),
+                value: variant.name,
+              };
+            }),
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(red("✖") + " Operation cancelled");
+        },
+      }
+    );
+  } catch (cancelled: any) {
+    console.log(cancelled.message);
+    return;
+  }
+
+  // user choice associated with prompts
+  const { framework, overwrite, packageName, variant } = result;
+
+  const root = path.join(cwd, targetDir);
+
+  if (overwrite) {
+    emptyDir(root);
+  } else if (!fs.existsSync(root)) {
+    fs.mkdirSync(root, { recursive: true });
+  }
+
+  // determine template
+  let template: string = variant || framework?.name || argTemplate;
+
+  const { customCommand } =
+    FRAMEWORKS.flatMap((f) => f.variants).find((v) => v.name === template) ??
+    {};
+
+  if (customCommand) {
+    const fullCustomCommand = customCommand;
+    const [command, ...args] = fullCustomCommand.split(" ");
+    // we replace TARGET_DIR here because targetDir may include a space
+    const replacedArgs = args.map((arg) =>
+      arg.replace("TARGET_DIR", targetDir)
+    );
+    const { status } = spawn.sync(command, replacedArgs, {
+      stdio: "inherit",
+    });
+    process.exit(status ?? 0);
+  }
+
+  console.log(`\nScaffolding project in ${root}...`);
+
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    "../../templates",
+    `template-${template}`
+  );
+
+  const write = (file: string, content?: string) => {
+    const targetPath = path.join(root, renameFiles[file] ?? file);
+    if (content) {
+      fs.writeFileSync(targetPath, content);
+    } else {
+      copy(path.join(templateDir, file), targetPath);
+    }
+  };
+
+  const files = fs.readdirSync(templateDir);
+  for (const file of files) {
+    write(file);
+  }
+  const projectName = packageName || getProjectName();
+
+  if (framework?.type === FrameworkType.node) {
+    setupNodeProject(write, templateDir, projectName);
+  }
+
+  const cdProjectName = path.relative(cwd, root);
+  console.log(`\nDone. Now run:\n`);
+  if (root !== cwd) {
+    console.log(
+      `  cd ${
+        cdProjectName.includes(" ") ? `"${cdProjectName}"` : cdProjectName
+      }`
+    );
+  }
+
+  console.log();
+}
+
+init().catch((e) => {
+  console.error(e);
+});
---
+++ src/templates.ts
@@ -0,0 +1,80 @@
+import {
+  blue,
+  cyan,
+  green,
+  lightGreen,
+  lightRed,
+  magenta,
+  red,
+  reset,
+  yellow,
+} from "kolorist";
+
+export enum FrameworkType {
+  node = "node",
+}
+
+export type ColorFunc = (str: string | number) => string;
+export type Framework = {
+  name: string;
+  display: string;
+  color: ColorFunc;
+  variants: FrameworkVariant[];
+  type?: keyof typeof FrameworkType;
+};
+export type FrameworkVariant = {
+  name: string;
+  display: string;
+  color: ColorFunc;
+  customCommand?: string;
+};
+
+export const FRAMEWORKS: Framework[] = [
+  {
+    name: "vanilla",
+    display: "Vanilla",
+    color: yellow,
+    type: "node",
+    variants: [
+      {
+        name: "vanilla-ts",
+        display: "TypeScript",
+        color: blue,
+      },
+      {
+        name: "vanilla",
+        display: "JavaScript",
+        color: yellow,
+      },
+    ],
+  },
+  {
+    name: "vite",
+    display: "Vite",
+    color: reset,
+    variants: [
+      {
+        name: "create-vite",
+        display: "create-vite ↗",
+        color: reset,
+        customCommand: "npm create vite@latest TARGET_DIR",
+      },
+      {
+        name: "create-vite-extra",
+        display: "create-vite-extra ↗",
+        color: reset,
+        customCommand: "npm create vite-extra@latest TARGET_DIR",
+      },
+      {
+        name: "create-electron-vite",
+        display: "create-electron-vite ↗",
+        color: reset,
+        customCommand: "npm create electron-vite@latest TARGET_DIR",
+      },
+    ],
+  },
+];
+
+export const TEMPLATES = FRAMEWORKS.map(
+  (f) => (f.variants && f.variants.map((v) => v.name)) || [f.name]
+).reduce((a, b) => a.concat(b), []);
